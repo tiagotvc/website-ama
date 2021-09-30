4f33f34a9b221f53dd8da2cf7f6d691a7636c3de
Tiago Vinicius Machado Carvalho <49541438+tiagotvc@users.noreply.github.com>
2021-09-30 10:57:57 -0300

    Dropdown editado

--- ama-front/src/components/DropDown/index.jsx
+++ ama-front/src/components/DropDown/index.jsx
@@ -6,48 +6,28 @@ import * as Styled from './styles';
 
 export const DropDown = ({ text }) => {
 
-    // eslint-disable-next-line no-unused-vars
-    const [handlerShow, setHandler] = useState(false);
+  const [handlerShow, setHandler] = useState(false);
+  const onClick = () => setHandler(!handlerShow);
 
     return (
+      <>
         <Styled.Container>
-           <span onMouseEnter={() => setHandler(true)} onMouseLeave={() => setHandler(false) }>{text}</span>
-           <Styled.SubDropDown show={handlerShow}>
-               <ul>
-                <Heading size='small'>Autismo</Heading>
-                   <li><a href='%%'>1. Primeiro</a></li>
-                   <li>2. Segundo</li>
-                   <li>3. Terceiro</li>
-                   <li>4. Quarto</li>
-               </ul>
-               <ul>
-               <Heading size='small'>Autismo</Heading>
-                   <li>1. Primeiro</li>
-                   <li>2. Segundo</li>
-                   <li>3. Terceiro</li>
-                   <li>4. Quarto</li>
-               </ul>
-               <ul>
-                <Heading size='small'>Autismo</Heading>
-                   <li>1. Primeiro</li>
-                   <li>2. Segundo</li>
-                   <li>3. Terceiro</li>
-                   <li>4. Quarto</li>
-               </ul>
-               <ul>
-               <Heading size='small'>Autismo</Heading>
-                   <li>1. Primeiro</li>
-                   <li>2. Segundo</li>
-                   <li>3. Terceiro</li>
-                   <li>4. Quarto</li>
-               </ul>
-               
-               
-            </Styled.SubDropDown>
+           <span  onClick={onClick}>{text}</span>
+
         </Styled.Container>
+         <Styled.SubDropDown  show={handlerShow}>
+         <ul>
+          <Heading size='small'>Autismo</Heading>
+             <li><a href='%%'>1. Primeiro</a></li>
+             <li>2. Segundo</li>
+             <li>3. Terceiro</li>
+             <li>4. Quarto</li>
+         </ul>
+      </Styled.SubDropDown>
+      </>
     );
 };
 
 DropDown.propTypes = {
-    text: P.string.isRequired, 
+    text: P.string.isRequired,
 }
--- ama-front/src/components/DropDown/styles.js
+++ ama-front/src/components/DropDown/styles.js
@@ -11,20 +11,29 @@ export const Container = styled.a`
         position: relative;
         cursor:pointer;
 
-       
+
     `}
 `;
 
 export const SubDropDown = styled.div`
     ${({ show }) => css`
-        display:${show ? 'flex' :'none'};
+        display:'flex';
         color:white;
         background:blue;
         position: absolute;
-        
+        visibility: ${ show ? 'visible' : 'hidden' };
+        opacity: ${ show ? 1 : 0 };
+        transition: opacity 0.4s ease, transform 0.4s ease, visibility 0.4s;
+        align-items: center;
+        justify-content: center;
+        left: 0;
+        right: 0;
+        top:6vh;
+
+
     > ul {
-        list-style: none;  
-        
+        list-style: none;
+
     }
     > ul > li > a {
         text-decoration: none;
@@ -35,7 +44,7 @@ export const SubDropDown = styled.div`
         }
     }
 
-   
+
     `}
 `;
 
